2d6bd4b0bbdd9b7d01f1f1891274c1f5fe959ab5
istianaAyu <istianaayu28@gmail.com>
2019-02-27 22:12:21 +0700

    fourth commit

--- app/src/main/java/com/example/recyclerviewtugas/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtugas/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.recyclerviewtugas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.recyclerviewtugas.adapter.AplikasiAdapter;
 import com.example.recyclerviewtugas.models.Aplikasi;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView rvAplikasi = (RecyclerView) findViewById(R.id.rvaplikasi);
 
-        aplikasi = aplikasi.createAplikasiList(20);
-        Aplikasi adapter = new Aplikasi(aplikasi);
-        rvAplikasi.setAdapter(adapter);
-        rvAplikasi.setLayoutManager(new LinearLayoutManager(this));
-    }
-}
+        List<Aplikasi> aplikasiList = new ArrayList<>();
+        aplikasiList.add(new Aplikasi("aplikasi_1", "WhatsApp","",R.drawable.wa));
+        aplikasiList.add(new Aplikasi("aplikasi_2", "Instagram","", R.drawable.Instagram));
+        aplikasiList.add(new Aplikasi("aplikasi_3", "Line","", R.drawable.line));
+        aplikasiList.add(new Aplikasi("aplikasi_4", "Webtoon","" ,R.drawable.webtoon));
+
+        AplikasiAdapter adapterAplikasi = new AplikasiAdapter(aplikasiList);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvAplikasi);
+        recyclerView.setAdapter(adapterAplikasi);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }}
 
--- app/src/main/java/com/example/recyclerviewtugas/adapter/AplikasiAdapter.java
+++ app/src/main/java/com/example/recyclerviewtugas/adapter/AplikasiAdapter.java
@@ -1,10 +1,70 @@
 package com.example.recyclerviewtugas.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.recyclerviewtugas.R;
+import com.example.recyclerviewtugas.models.Aplikasi;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class AplikasiAdapter extends RecyclerView.Adapter<AplikasiAdapter.ViewHolder> {
+
+    private List<Aplikasi> AplikasiList;
+
+    public AplikasiAdapter(List<Aplikasi> AplikasiList) {
+
+        this.AplikasiList = AplikasiList;
+    }
+
     @NonNull
     @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
+        View filmView = inflater.inflate(R.layout.item_aplikasi, viewGroup, false);
+
+        return new ViewHolder(filmView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Aplikasi Aplikasi = AplikasiList.get(i);
+
+        if (Aplikasi.getImg() != null) {
+            Picasso.get().load(Aplikasi.getImg()).into(viewHolder.imageView);
+        } else {
+            Picasso.get().load(Aplikasi.getImg_local()).into(viewHolder.imageView);
+        }
+
+        viewHolder.textView.setText(Aplikasi.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return AplikasiList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView textView;
+        TextView descView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.item_aplikasi_img);
+            textView = itemView.findViewById(R.id.item_aplikasi_title);
+            descView = itemView.findViewById(R.id.item_aplikasi_desc);
+        }
+    }
 }
